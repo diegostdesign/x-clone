4dd89126f6239e0968937bf3cf75ee46337d28de
Diego Soto <diegostdesign@gmail.com>
2024-02-03 18:21:30 -0700

    Commit initial files

---
+++ data.js
@@ -0,0 +1,19 @@
+export const tweetsData = [   
+    {
+        handle: `@BlueSkyTraveler`,
+        profilePic: `images/travelbus.png`,
+        likes: 37,
+        retweets: 6,
+        tweetText: `Exploring new horizons under the blue sky. 🌍✈️ Discovering places that inspire and connect. #TravelDreams #Wanderlust`,
+        replies: [
+                  {
+                handle: `@AdventureSeeker23`,
+                profilePic: `images/travelpost.png`,
+                tweetText: `Sounds like an incredible journey ✨ Where's your next destination? #AdventureAwaits #TravelGoals`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+]
